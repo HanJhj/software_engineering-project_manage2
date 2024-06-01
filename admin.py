0c3767b36e06a6111c8864f37d231f05dac941de
HanJhj <2331772430@qq.com>
2024-06-01 20:47:36 +0800

    C4 branch modifications

--- admin.py
+++ admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 
 # Register your models here【change 1】.
 # 分支管理修改-B2
+# 分支修改C4
--- models.py
+++ models.py
@@ -1,3 +1,11 @@
+'''
+Author: HanJ
+Date: 2024-06-01 20:32:44
+LastEditors: Do not edit
+LastEditTime: 2024-06-01 20:43:58
+FilePath: \undefinedd:\Documents\myStudy\software_project\personal_homework4\software_engineering-project_manage2\models.py
+'''
 from django.db import models
 
 # Create your models here.
+# 分支修改C4
